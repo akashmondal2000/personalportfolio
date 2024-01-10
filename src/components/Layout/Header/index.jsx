924d1c6200480caf8c9df48cbac9ed563dad646a
akashmondal2000 <mondalakash494@gmail.com>
2024-01-10 16:12:59 +0530

    install tailwind css and create header

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import styles from "./header.module.css";
 
 const CustomHeader = () => {
   return (
-    <div>CustomHeader</div>
-  )
-}
+    <section className={styles.headerMainSection}>
+      {/* image section  */}
+      <section className={styles.imageSectionWrapper}>
+        <div className={styles.imageWrapper}>
+          <img
+            className={styles.imageLogo}
+            src="https://cdn.pixabay.com/photo/2023/03/23/07/56/car-7871392_1280.jpg"
+            alt="logo"
+          />
+        </div>
+      </section>
+      {/* menu  */}
+      <section className={styles.manuSectionContainer}>
+      <div className={styles.menuWrapper}>
+      <p>Home</p>
+      <p>About</p>
+      <p>Skills</p>
+      <p>Contact</p>
+      </div>
+      </section>
+      
+    </section>
+  );
+};
 
 export default CustomHeader;
